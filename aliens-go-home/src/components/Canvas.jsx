fcc965b2ee0bfae713b60dd6f16295ebf0c1a391
Sleet01 <sleet01@hotmail.com>
2019-07-05 18:22:20 -0700

    Refactored to have game states, clicking on Start button starts game.

--- aliens-go-home/src/components/Canvas.jsx
+++ aliens-go-home/src/components/Canvas.jsx
@@ -7,9 +7,10 @@ import CannonPipe from './CannonPipe';
 import CannonBall from './CannonBall';
 import CurrentScore from './CurrentScore';
 import FlyingObject from './FlyingObject';
-import Heart from './Heart';
-import StartGame from './StartGame';
+//import Heart from './Heart';
+//import StartGame from './StartGame';
 import Title from './Title';
+import StartGame from "./StartGame";
 
 const Canvas = (props) => {
     const gameHeight = 1200;
@@ -32,18 +33,33 @@ const Canvas = (props) => {
             <CannonBase />
             <CannonBall position={{x: 0, y: -100}}/>
             <CurrentScore score={15} />
-            <FlyingObject position={{x: -150, y: -300}}/>
-            <FlyingObject position={{x: 150, y: -300}}/>
-            <Heart position={{x: -300, y: 35}} />
-            <StartGame onClick={() => console.log('Aliens, Go Home!')} />
-            <Title />
+
+            { ! props.gameState.started &&
+                <g>
+                    <StartGame onClick={() => props.startGame()} />
+                    <Title />
+                </g>
+            }
+
+            { props.gameState.started &&
+                <g>
+                    <FlyingObject position={{x: -150, y: -300}}/>
+                    <FlyingObject position={{x: 150, y: -300}}/>
+                </g>
+            }
         </svg>
     );
 };
 
 Canvas.propTypes = {
     angle: PropTypes.number.isRequired,
+    gameState: PropTypes.shape({
+        started: PropTypes.bool.isRequired,
+        kills: PropTypes.number.isRequired,
+        lives: PropTypes.number.isRequired,
+    }).isRequired,
     trackMouse: PropTypes.func.isRequired,
+    startGame: PropTypes.func.isRequired,
 };
 
 export default Canvas;
